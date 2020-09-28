7c55698080616849996f901f23f0b290c5c269cd
michalklos <galeo09@gmail.com>
2020-09-28 17:16:08 +0200

    dynamodb config and handler

--- handlers/afterFileUpload.js
+++ handlers/afterFileUpload.js
@@ -1,57 +1,29 @@
 'use strict';
 const AWS = require('aws-sdk');
+AWS.config.update({ region: "eu-west-1"});
 
-AWS.config.update({region: 'eu-west-1'});
+module.exports.afterFileUpload = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "eu-west-1"});
 
-const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
-const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Credentials': true,
-};
-
-module.exports.afterFileUpload = async (event, context, callback) => {
   const identity = event.Records[0].userIdentity;
   const s3 = event.Records[0].s3;
+
 
   const params = {
     TableName: 'beta-keys',
     Item: {
-      'userId': {S: identity.principalId},
-      'imgKey': {S: s3.object.key},
+      userId: identity.principalId,
+      imgKey: s3.object.key,
     }
   }
 
-  ddb.putItem(params, (err, data) => {
-    if (err) {
-      return {
-        headers,
-        statusCode: 200,
-        body: JSON.stringify(
-          {
-            message: err,
-            input: event,
-          },
-          null,
-          2
-        ),
-      };
-    } else {
-      return {
-        headers,
-        statusCode: 200,
-        body: JSON.stringify(
-          {
-            message: 'Done',
-            input: event,
-          },
-          null,
-          2
-        ),
-      };
-    }
-  })
-
+  try {
+    const data = await documentClient.put(params).promise();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
